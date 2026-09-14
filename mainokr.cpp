c588449a9af1f75a2e9104e0bbf0b90e490366e7
vi-vipo <vitalijpodoplelov@gmail.com>
2026-09-14 09:32:18 +0700

    измененный

--- mainokr.cpp
+++ mainokr.cpp
@@ -9,8 +9,12 @@ int main() {
 	cin >> radius;
 	cout << "Введите угол:";
 	cin >> ugol;
-	cout << "Площадь:" << 3.14 * radius * radius << endl;
-	cout << "Площадь сектора:" << ugol * 3.14 * radius * radius / 360 << endl;
-	cout << "Длина:" << 2 * 3.14 * radius << endl;
-	
+	if ((radius > 0) && (ugol <= 360) && (ugol > 0)){
+	        cout << "Площадь:" << 3.14 * radius * radius << endl;
+	        cout << "Длина:" << 2 * 3.14 * radius << endl;
+		cout << "Площадь сектора:" << ugol * 3.14 * radius * radius / 360 << endl;
+  }
+      else{
+      cout << "ошибка" << endl;
+      }
 }
